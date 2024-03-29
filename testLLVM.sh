9e003ffd0e6e695d3fc28caabe89147d225cd020
howldark <2405902275@qq.com>
2024-03-29 10:18:05 +0800

    修改脚本

--- testLLVM.sh
+++ testLLVM.sh
@@ -1,11 +1,9 @@
-xmake run irTest $1 > main.ll
+xmake run irtest $1 > main.ll
 llvm-as main.ll -o main.bc
 llvm-link -o output.bc main.bc ./lib/sylib.bc
 
 lli output.bc
 
-echo ""
-echo "return value:"
-echo $?
+echo -e "\nreturn_val:\n"$?
 
 rm -rf main.bc output.bc
