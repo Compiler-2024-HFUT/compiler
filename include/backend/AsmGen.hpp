06d5758e0e3cb9cfcc24696aa4c3189af341b2e6
MCQ <1614913642@qq.com>
2024-07-03 10:23:30 +0800

    命名修改AsmPrint->AsmString

--- include/backend/AsmGen.hpp
+++ include/backend/AsmGen.hpp
@@ -31,7 +31,32 @@ storeoffsetinst
 */
 
 class AsmGen : public IRVisitor{
-
+    public:
+        AsmGen();
+    
+    private:
+        virtual void visit(Module &node) override;
+        virtual void visit(Function &node) override;
+        virtual void visit(BasicBlock &node) override;
+        virtual void visit(BinaryInst &node) override;
+        virtual void visit(CmpInst &node) override;
+        virtual void visit(FCmpInst &node) override;
+        virtual void visit(CallInst &node) override;
+        virtual void visit(BranchInst &node) override;
+        virtual void visit(ReturnInst &node) override;
+        virtual void visit(GetElementPtrInst &node) override;
+        virtual void visit(StoreInst &node) override;
+        virtual void visit(MemsetInst &node) override;
+        virtual void visit(LoadInst &node) override;
+        virtual void visit(AllocaInst &node) override;
+        virtual void visit(ZextInst &node) override;
+        virtual void visit(SiToFpInst &node) override;
+        virtual void visit(FpToSiInst &node) override;
+        virtual void visit(PhiInst &node) override;
+        virtual void visit(CmpBrInst &node) override;
+        virtual void visit(FCmpBrInst &node) override;
+        virtual void visit(LoadOffsetInst &node) override;
+        virtual void visit(StoreOffsetInst &node) override;
 
 
 
