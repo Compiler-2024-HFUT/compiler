66772b0f626954f052bc42076c21cb3fc544bcca
howldark <2405902275@qq.com>
2024-07-22 15:10:08 +0800

    活跃变量fixbug

--- src/analysis/LiveVar.cpp
+++ src/analysis/LiveVar.cpp
@@ -5,10 +5,6 @@
 #include "midend/Instruction.hpp"
 #include "utils/Logger.hpp"
 
-#include <algorithm>
-using std::set_union;
-using std::set_difference;
-
 void LiveVar::analyseOnFunc(Function *func_) {
     initUseAndDef(func_);
     findFixedPoint(func_);
@@ -20,55 +16,35 @@ void LiveVar::initUseAndDef(Function *func_) {
         UEUses[bb] = {};
         PhiDefs[bb] = {}; 
         PhiUses[bb] = {}; 
-    }
 
-    for(BB *bb : func_->getBasicBlocks()) {
         for(Instruction *inst : bb->getInstructions()) {
             // def
             if( !inst->isVoid() ) {
+                Defs[bb].insert(inst);
                 if( inst->isPhi() ) {
                     PhiDefs[bb].insert(inst);
-                } else {
-                    Defs[bb].insert(inst);
                 }
             }
             
-            // use
-            if( inst->isPhi() ) {
-                for(int i=0; i<inst->getOperands().size(); i+=2) {
-                    Value *op = inst->getOperand(i);
-                    if (dynamic_cast<GlobalVariable *>(op) || dynamic_cast<Constant*>(op) ||
-                        dynamic_cast<BasicBlock*>(op) || dynamic_cast<Function*>(op) ) continue;
-                    
-                    PhiUses[bb].insert( op );
-                }
-            } else {
+            // UEUses
+            if( !inst->isPhi() ) {
                 for(Value *op : inst->getOperands()) {
                     if (dynamic_cast<GlobalVariable *>(op) || dynamic_cast<Constant*>(op) ||
                         dynamic_cast<BasicBlock*>(op) || dynamic_cast<Function*>(op) ) continue;
 
-                    if( !Defs[bb].count(op) && !PhiDefs[bb].count(op) ) {
+                    if( !Defs[bb].count(op) ) {
                         UEUses[bb].insert(op);
                     }
                 }
             }
         }
     }
-
 }
 
 void LiveVar::findFixedPoint(Function *func_) {
     for(BB *bb : func_->getBasicBlocks()) {
         liveIn[bb] = {{}, {}};
         liveOut[bb] = {{}, {}};
-
-        for(Value *v : PhiDefs[bb]) {
-            if(v->getType()->isFloatType()) {
-                liveIn[bb].second.insert(v);
-            } else {
-                liveIn[bb].first.insert(v);
-            }
-        }
 
         for(Value *v : UEUses[bb]) {
             if(v->getType()->isFloatType()) {
@@ -77,50 +53,81 @@ void LiveVar::findFixedPoint(Function *func_) {
                 liveIn[bb].first.insert(v);
             }
         }
-
-        for(Value *v : PhiUses[bb]) {
-            if(v->getType()->isFloatType()) {
-                liveOut[bb].second.insert(v);
-            } else {
-                liveOut[bb].first.insert(v);
-            }
-        }
-        
     }
     
     bool changed = true;
     while(changed) {
         changed = false;
         for(BB *bb : func_->getBasicBlocks()) {
-            // int
             for(BB *succ : bb->getSuccBasicBlocks()) {
+                // int
                 for(Value *v : liveIn[succ].first) {
                     if(!PhiDefs[succ].count(v) && !liveOut[bb].first.count(v)) {
                         changed = true;
                         liveOut[bb].first.insert(v);
 
-                        if(!Defs[bb].count(v)) {
+                        if( !Defs[bb].count(v) ) {
                             liveIn[bb].first.insert(v);
                         }
                     }
                 }
-            }
             
-            // float
-            for(BB *succ : bb->getSuccBasicBlocks()) {
+                // float
                 for(Value *v : liveIn[succ].second) {
                     if(!PhiDefs[succ].count(v) && !liveOut[bb].second.count(v)) {
                         changed = true;
                         liveOut[bb].second.insert(v);
 
-                        if(!Defs[bb].count(v)) {
+                        if( !Defs[bb].count(v) ) {
                             liveIn[bb].second.insert(v);
+                        }
+                    }
+                }
+
+                // PhiUse
+                for(Value *succPhiV : PhiDefs[succ]) {
+                    Instruction *succPhiI = dynamic_cast<PhiInst*>(succPhiV);
+                    for(int i = 0; i < succPhiI->getNumOperands(); i += 2) {
+                        BasicBlock *pre = dynamic_cast<BasicBlock*>(succPhiI->getOperand(i+1));
+                        Value *op = succPhiI->getOperand(i);
+
+                        if(op->getType()->isFloatType()) {
+                            if(!liveOut[bb].second.count(op) && pre == bb) {
+                                changed = true;
+                                liveOut[bb].second.insert(op);
+                                if( !Defs[bb].count(op) ) {
+                                    liveIn[bb].second.insert(op);
+                                }
+                            }
+                        } else {
+                            if(!liveOut[bb].first.count(op) &&  pre == bb) {
+                                changed = true;
+                                liveOut[bb].first.insert(op);
+                                if( !Defs[bb].count(op) ) {
+                                    liveIn[bb].first.insert(op);
+                                }
+                            }
                         }
                     }
                 }
             }
         }
     }
+
+    for(BasicBlock *bb : func_->getBasicBlocks()) {
+        for(Value *phiV : PhiDefs[bb]) {
+            Instruction *phiI = dynamic_cast<PhiInst*>(phiV);
+            for(int i = 0; i < phiI->getNumOperands(); i += 2) {
+                Value *op = phiI->getOperand(i);
+                if(op->getType()->isFloatType()) {
+                    liveIn[bb].second.erase(op);
+                } else {
+                    liveIn[bb].first.erase(op);
+                }
+            }
+        }
+    }
+
 }
 
 string LiveVar::print() {
