63c405d810ad977f9dbbb259018e0ca48945eed3
jqran <aran13640@outlook.com>
2024-07-30 15:58:56 +0800

    fix:重复load

--- include/optimization/ArrReduc.hpp
+++ include/optimization/ArrReduc.hpp
@@ -2,22 +2,29 @@
 #define ARR_REDUC_HPP
 #include "analysis/Info.hpp"
 #include "analysis/InfoManager.hpp"
+#include "analysis/funcAnalyse.hpp"
 #include "midend/BasicBlock.hpp"
 #include "midend/Function.hpp"
 #include "midend/Instruction.hpp"
 #include "midend/Module.hpp"
+#include "midend/Value.hpp"
 #include "optimization/PassManager.hpp"
 #include <map>
 #include <set>
 class ArrReduc:public FunctionPass{
 private:
+    void init()override{
+        funca=info_man_->getInfo<FuncAnalyse>();
+    }
+    FuncAnalyse *funca;
     std::set<BasicBlock*>visited;
     std::map<GetElementPtrInst*,LoadInst*> gep_load;
 public:
     bool recurGep(AllocaInst*alloc,GetElementPtrInst* gep);
     bool canDelete(AllocaInst*alloc);
-    Modify runOnFunc(Function*func);
-    Modify runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load);
+    virtual Modify runOnFunc(Function*func) override;
+    Modify runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load,std::map<GetElementPtrInst*,Value*>gep_curval);
+    Modify runOnBBConst(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load,std::map<GetElementPtrInst*,Value*>gep_curval);
     ArrReduc(Module*m,InfoManager *im):FunctionPass(m,im){}
 };
 #endif
--- src/optimization/ArrReduc.cpp
+++ src/optimization/ArrReduc.cpp
@@ -1,13 +1,44 @@
 #include "optimization/ArrReduc.hpp"
 #include "analysis/Info.hpp"
+#include "midend/Constant.hpp"
+#include "midend/Function.hpp"
 #include "midend/Instruction.hpp"
+#include "midend/Value.hpp"
 Modify ArrReduc::runOnFunc(Function*func){
     if(func->isDeclaration())
         return {};
     visited.clear();
-    return runOnBB(func->getEntryBlock(),{});
+    auto ret= runOnBB(func->getEntryBlock(),{},{});
+    visited.clear();
+    return ret|runOnBBConst(func->getEntryBlock(),{},{});
 }
-Modify ArrReduc::runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load){
+void store_rm(Value*lval,std::map<GetElementPtrInst*,LoadInst*> &gep_load,std::map<GetElementPtrInst*,Value*>&gep_curval){
+    while(dynamic_cast<GetElementPtrInst*>(lval)){
+        lval=((GetElementPtrInst*)lval)->getOperand(0);
+    }
+    for (auto it = gep_load.begin();it != gep_load.end();) {
+		auto gep_ptr=it->first->getOperand(0);
+        while(dynamic_cast<GetElementPtrInst*>(gep_ptr)){
+            gep_ptr=((GetElementPtrInst*)gep_ptr)->getOperand(0);
+        }
+        if(gep_ptr==lval)
+			it = gep_load.erase(it);
+        else
+			++it;
+    }
+    for (auto it = gep_curval.begin();it != gep_curval.end();) {
+		auto gep_ptr=it->first->getOperand(0);
+        while(dynamic_cast<GetElementPtrInst*>(gep_ptr)){
+            gep_ptr=((GetElementPtrInst*)gep_ptr)->getOperand(0);
+        }
+		if (gep_ptr==lval)
+			it = gep_curval.erase(it);
+		else
+			++it;
+    }
+
+}
+Modify ArrReduc::runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load,std::map<GetElementPtrInst*,Value*>gep_curval){
     Modify ret;
     visited.insert(bb);
     auto &ins_list=bb->getInstructions();
@@ -19,8 +50,17 @@ Modify ArrReduc::runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> ge
             auto lval=load->getLVal();
             if(auto gep=dynamic_cast<GetElementPtrInst*>(lval)){
                 auto gep_iter=gep_load.find(gep);
+                auto gepval_iter=gep_curval.find(gep);
+                if(gepval_iter!=gep_curval.end()){
+                    load->replaceAllUseWith(gepval_iter->second);
+                    load->removeUseOfOps();
+                    __iter=ins_list.erase(cur_iter);
+                    ret.modify_instr=true;
+                    continue;
+                }
                 if(gep_iter!=gep_load.end()){
                     load->replaceAllUseWith(gep_iter->second);
+                    load->removeUseOfOps();
                     __iter=ins_list.erase(cur_iter);
                     ret.modify_instr=true;
                 }else{
@@ -31,9 +71,28 @@ Modify ArrReduc::runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> ge
             auto store=(StoreInst*)instr;
             auto lval=store->getLVal();
             if(auto gep=dynamic_cast<GetElementPtrInst*>(lval)){
+                if(gep->getNumOperands()==3){
+                    if(dynamic_cast<ConstantInt*>(gep->getOperand(2))){
+                        store_rm(gep->getOperand(0),gep_load,gep_curval);
+                        continue;
+                    }
+                }else{
+                    if(dynamic_cast<ConstantInt*>(gep->getOperand(1))){
+                        store_rm(gep->getOperand(0),gep_load,gep_curval);
+                        continue;
+                    }
+                }
                 auto gep_iter=gep_load.find(gep);
                 if(gep_iter!=gep_load.end()){
                     gep_load.erase(gep_iter);
+                }
+                gep_curval.insert({gep,store->getRVal()});
+            }
+        }else if(instr->isCall()){
+            if(auto iter=funca->all_se_info.find((Function*)instr->getOperand(0));iter!=funca->all_se_info.end()){
+                if(iter->second.isWriteGlobal()||iter->second.isWriteParamArray()){
+                    gep_load.clear();
+                    gep_curval.clear();
                 }
             }
         }
@@ -42,9 +101,80 @@ Modify ArrReduc::runOnBB(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> ge
         if(visited.count(succ_bb))
             continue;
         if(succ_bb->getPreBasicBlocks().size()==1&&succ_bb->getPreBasicBlocks().front()==bb){
-            ret=ret|runOnBB(succ_bb,gep_load);
+            ret=ret|runOnBB(succ_bb,gep_load,gep_curval);
         }else{
-            ret=ret|runOnBB(succ_bb,{});
+            ret=ret|runOnBB(succ_bb,{},{});
+        }
+    }
+    return ret;
+}
+Modify ArrReduc::runOnBBConst(BasicBlock*bb,std::map<GetElementPtrInst*,LoadInst*> gep_load,std::map<GetElementPtrInst*,Value*>gep_curval){
+    Modify ret;
+    visited.insert(bb);
+    auto &ins_list=bb->getInstructions();
+    for(auto __iter=ins_list.begin();__iter!=ins_list.end();){
+        auto cur_iter=__iter++;
+        auto instr=*cur_iter;
+        if(instr->isLoad()){
+            auto load=(LoadInst*)instr;
+            auto lval=load->getLVal();
+            if(auto gep=dynamic_cast<GetElementPtrInst*>(lval)){
+                auto gep_iter=gep_load.find(gep);
+                auto gepval_iter=gep_curval.find(gep);
+                if(gepval_iter!=gep_curval.end()){
+                    load->replaceAllUseWith(gepval_iter->second);
+                    load->removeUseOfOps();
+                    __iter=ins_list.erase(cur_iter);
+                    ret.modify_instr=true;
+                    continue;
+                }
+                if(gep_iter!=gep_load.end()){
+                    load->replaceAllUseWith(gep_iter->second);
+                    load->removeUseOfOps();
+                    __iter=ins_list.erase(cur_iter);
+                    ret.modify_instr=true;
+                }else{
+                    gep_load.insert({gep,load});
+                }
+            }
+        }else if(instr->isStore()){
+            auto store=(StoreInst*)instr;
+            auto lval=store->getLVal();
+            if(auto gep=dynamic_cast<GetElementPtrInst*>(lval)){
+                if(gep->getNumOperands()==3){
+                    if(dynamic_cast<ConstantInt*>(gep->getOperand(2))==0){
+                        store_rm(gep->getOperand(0),gep_load,gep_curval);
+                        continue;
+                    }
+                }else{
+                    if(dynamic_cast<ConstantInt*>(gep->getOperand(1))==0){
+                        store_rm(gep->getOperand(0),gep_load,gep_curval);
+                        continue;
+                    }
+
+                }
+                auto gep_iter=gep_load.find(gep);
+                if(gep_iter!=gep_load.end()){
+                    gep_load.erase(gep_iter);
+                }
+                gep_curval.insert({gep,store->getRVal()});
+            }
+        }else if(instr->isCall()){
+            if(auto iter=funca->all_se_info.find((Function*)instr->getOperand(0));iter!=funca->all_se_info.end()){
+                if(iter->second.isWriteGlobal()||iter->second.isWriteParamArray()){
+                    gep_load.clear();
+                    gep_curval.clear();
+                }
+            }
+        }
+    }
+    for(auto succ_bb:bb->getSuccBasicBlocks()){
+        if(visited.count(succ_bb))
+            continue;
+        if(succ_bb->getPreBasicBlocks().size()==1&&succ_bb->getPreBasicBlocks().front()==bb){
+            ret=ret|runOnBBConst(succ_bb,gep_load,gep_curval);
+        }else{
+            ret=ret|runOnBBConst(succ_bb,{},{});
         }
     }
     return ret;
