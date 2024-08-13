01a43a1e3b3edefc83646719c796e92595757a81
howldark <2405902275@qq.com>
2024-08-13 12:45:24 +0800

    支配性问题

--- include/optimization/GCM.hpp
+++ include/optimization/GCM.hpp
@@ -1,6 +1,7 @@
 /*
     Global Code Motion
     https://roife.github.io/posts/click1995/
+    https://courses.cs.washington.edu/courses/cse501/06wi/reading/click-pldi95.pdf
 */
 
 #ifndef GCM_HPP
@@ -17,15 +18,16 @@ using std::set;
 class GCM : public ModulePass {
     umap<BasicBlock*, int> domDepth;
     umap<BasicBlock*, int> loopDepth;
-    umap<Instruction*, BasicBlock*> earlyBB;
     set<Instruction*> visited;
+    umap<Instruction*, BasicBlock*> earBB;
+    umap<Instruction*, BasicBlock*> latBB;
     
     bool isPinned(Instruction *inst);
     void computeDepths(Function *func);
     // find the nearest common ancestor(LCA) of two basic blocks
     BasicBlock *findLCA(BasicBlock *bb1, BasicBlock *bb2);  
-    void scheduleEarly(Instruction *inst);
-    bool scheduleLate(Instruction *inst);
+    Instruction *scheduleEarly(Instruction *inst);
+    Instruction *scheduleLate(Instruction *inst);
     bool visitFunction(Function *func);
 public:
     GCM(Module *m,InfoManager *im): ModulePass(m, im){}
--- src/optimization/GCM.cpp
+++ src/optimization/GCM.cpp
@@ -1,9 +1,11 @@
 #include "optimization/GCM.hpp"
+#include "utils/Logger.hpp"
+
+#include <list>
+using std::list;
 
 bool GCM::isPinned(Instruction *inst) {
-    return (inst->isBinary() || inst->isCmp() || inst->isFCmp() || 
-            inst->isGep() || inst->isZext() || 
-            inst->isFptosi() || inst->isSitofp());
+    return (inst->isPhi() || inst->isTerminator() || inst->isStore() || inst->isLoad() || inst->isCall());
 }
 
 void GCM::computeDepths(Function *func) {
@@ -16,11 +18,8 @@ void GCM::computeDepths(Function *func) {
     vector<Loop*> loops = loopinfo->getLoops(func);
     BasicBlock *root = func->getEntryBlock();
 
+    domDepth[root] = 0;
     std::function<void(BasicBlock*)> DFSDomTree = [&](BasicBlock *bb) {
-        // start from 0
-        if(domDepth.count(bb) == 0)
-            domDepth[bb] = 0;
-        
         for(BasicBlock *child : domTree[bb]) {
             domDepth[child] = domDepth[bb] + 1;
             DFSDomTree(child);
@@ -50,126 +49,151 @@ void GCM::computeDepths(Function *func) {
 }
 
 BasicBlock *GCM::findLCA(BasicBlock *bb1, BasicBlock *bb2) {
+    if(bb1 == nullptr)  return bb2;
+    
     Dominators *dom = info_man_->getInfo<Dominators>();
     map<BasicBlock*, BasicBlock*> idom = dom->getIDom();
 
-    if(bb1 == nullptr)  return bb2;
-    if(bb2 == nullptr)  return bb1;
-
-    while(domDepth[bb1] < domDepth[bb2])
-        bb1 = idom[bb1];
     while(domDepth[bb1] > domDepth[bb2])
+        bb1 = idom[bb1];
+    while(domDepth[bb1] < domDepth[bb2])
         bb2 = idom[bb2];
-    while(bb1!= bb2) {
+    while(bb1 != bb2) {
         bb1 = idom[bb1];
         bb2 = idom[bb2];
     }
+    LOG_ERROR("lca can't be null", !bb1)
     return bb1;
 }
 
-void GCM::scheduleEarly(Instruction *inst) {
+Instruction *GCM::scheduleEarly(Instruction *inst) {
     if(visited.count(inst))
-        return;
+        return inst;
     visited.insert(inst);
 
-    earlyBB[inst] = inst->getParent();
+    BasicBlock *instBB = inst->getFunction()->getEntryBlock();
     for(Value *op : inst->getOperands()) {
-        Instruction *opInst = dynamic_cast<Instruction*>(op);
-        if(!opInst)
-            continue;
-
-        scheduleEarly(opInst);
-        BasicBlock *opBB = opInst->getParent();
-        if(domDepth[opBB] < domDepth[ earlyBB[inst] ]) {
-            earlyBB[inst] = opBB;
-        }
-    }
-}
-
-
-bool GCM::scheduleLate(Instruction *inst) {
-    if(visited.count(inst))
-        return false;
-    visited.insert(inst);
-
-    bool changed = false;
-    BasicBlock *lca = nullptr;
-    for(Use user : inst->getUseList()) {
-        Instruction *userInst = dynamic_cast<Instruction*>(user.val_);
-        if(!userInst)
-            continue;
-
-        changed |= scheduleLate(userInst);
-        BasicBlock *userBB = userInst->getParent();
-        if(userInst->isPhi()) {
-            vector<Value*> &ops = userInst->getOperands();
-            for(int i = 1; i < ops.size(); i += 2) {
-                userBB = dynamic_cast<BasicBlock*>(ops[i]);
-                lca = (lca) ? findLCA(lca, userBB) : userBB;
+        if( Instruction *input = dynamic_cast<Instruction*>(op) ) {
+            input = scheduleEarly(input);
+            // from shallow to deep
+            if(domDepth[ instBB ] < domDepth[ input->getParent() ]) {
+                instBB = input->getParent();
             }
         }
-        lca = (lca) ? findLCA(lca, userBB) : userBB;
     }
+
+    LOG_WARNING("earlyBB: " + inst->getName() + ", " + instBB->getName())
+
+    if(!isPinned(inst) && inst->getParent() != instBB) {
+        inst->getParent()->deleteInstr(inst);
+        instBB->addInstrBeforeTerminator(inst);
+    }
+    
+    return inst;
+}
+
+Instruction *GCM::scheduleLate(Instruction *inst) {
+    if(visited.count(inst))
+        return inst;
+    visited.insert(inst);
 
     Dominators *dom = info_man_->getInfo<Dominators>();
     map<BasicBlock*, BasicBlock*> idom = dom->getIDom();
+
+    BasicBlock *lca = nullptr;
+    list<Use> uselist = list<Use>(inst->getUseList().begin(), inst->getUseList().end());
+    for(Use user : uselist) {
+        if(Instruction *userInst = dynamic_cast<Instruction*>(user.val_)) {
+            userInst = scheduleLate(userInst);
+            BasicBlock *userBB = userInst->getParent();
+            if(userInst->isPhi()) {
+                vector<Value*> &ops = userInst->getOperands();
+                for(int i = 0; i < ops.size(); i += 2) {
+                    if(ops[i] == inst)
+                        userBB = dynamic_cast<BasicBlock*>(ops[i+1]);
+                        break;
+                }
+            }
+            lca = findLCA(lca, userBB);
+        }
+    }
+    
+    LOG_WARNING("last: " + inst->getName() + ", " + lca->getName())
+
     BasicBlock *bestBB = lca;
-    BasicBlock *curBB = lca;
-    while(domDepth[curBB] >= domDepth[ earlyBB[inst] ]) {
-        if(loopDepth[curBB] < loopDepth[bestBB])
-            bestBB = curBB;
-        curBB = idom[curBB];
+    BasicBlock *instBB = inst->getParent();
+    while(lca != nullptr && lca != instBB) {
+        if(loopDepth[lca] < loopDepth[bestBB])
+            bestBB = lca;
+        lca = idom[lca];
+    }
+    if(!lca) return inst;
+
+    uselist = list<Use>(inst->getUseList().begin(), inst->getUseList().end());
+    for(Use user : uselist) {
+        if(Instruction *userInst = dynamic_cast<Instruction*>(user.val_)) {
+            if(!isPinned(inst) && userInst->getParent() == bestBB && !userInst->isPhi()) {
+                list<Instruction*> &userBBInstList = userInst->getParent()->getInstructions();
+                auto pos = find(userBBInstList.begin(), userBBInstList.end(), userInst);
+                
+                inst->getParent()->deleteInstr(inst);
+                inst->setParent(userInst->getParent());
+                userInst->getParent()->addInstruction(pos, inst);
+                return inst;
+            }
+        }
     }
 
-    if(bestBB != inst->getParent()) {
+    if(!isPinned(inst) && bestBB != instBB) {
         inst->getParent()->deleteInstr(inst);
         bestBB->addInstrBeforeTerminator(inst);
-        changed = true;
     }
-    return changed;
+    return inst;
 }
 
 bool GCM::visitFunction(Function *func) {
     bool changed = false;
     computeDepths(func);
-    
-    visited.clear();
 
     // ScheduleEarly
+    visited.clear();
     for(BasicBlock *bb : func->getBasicBlocks()) {
         for(Instruction *inst : bb->getInstructions()) {
             if(isPinned(inst)) {
                 visited.insert(inst);
                 for(Value *op : inst->getOperands()) {
-                    Instruction *opInst = dynamic_cast<Instruction*>(op);
-                    if(!opInst)
-                        continue;
-                    scheduleEarly(opInst);
+                    if(Instruction *input = dynamic_cast<Instruction*>(op))
+                        scheduleEarly(input);
                 }
             }
         }
     }
 
     // ScheduleLate
+    visited.clear();
     for(BasicBlock *bb : func->getBasicBlocks()) {
-        for(Instruction *inst : bb->getInstructions()) {
+        list<Instruction*> backupInsts = list<Instruction*>(bb->getInstructions().begin(), bb->getInstructions().end());
+        for(Instruction *inst : backupInsts) {
             if(isPinned(inst)) {
                 visited.insert(inst);
-                for(Use user : inst->getUseList()) {
-                    Instruction *userInst = dynamic_cast<Instruction*>(user.val_);
-                    if(!userInst)
-                        continue;
-                    changed |= scheduleLate(userInst);
+                list<Use> uselist = list<Use>(inst->getUseList().begin(), inst->getUseList().end());
+                for(Use user : uselist) {
+                    if(Instruction *userInst = dynamic_cast<Instruction*>(user.val_))
+                        scheduleLate(userInst);
                 }
             }
         }
     }
+
     return changed;
 }
 
 Modify GCM::runOnModule(Module *m) {
     Modify ret{};
     for(Function *func : m->getFunctions()) {
+        if(func->getBasicBlocks().size() == 0)
+            continue;
+
         if( visitFunction(func) ) 
             ret.modify_instr = true;
     }
